e98caebd69ee01da85d9afd7e0381b51c3d1a1b9
Yusuke Oya <curio@antique-cafe.net>
2023-02-21 10:53:19 +0800

    Update main.py
    fix

--- src/main.py
+++ src/main.py
@@ -9,12 +9,12 @@ openai.api_key = "YOUR_API_KEY"
 # STEP1: テキストを分散表現に変換する。
 BuildEmbeddingUseCase().execute(
     text_dir_path="text/fictional_novel",
-    embeddings_file_path="embeddings/fictional_novel.csv"
+    text_embeddings_file_path="embeddings/fictional_novel.csv"
 )
 
 # STEP2: テキストに対して質問を投げかけ、回答を得る。
 answer = AnswerQuestionUseCase().execute(
-    embeddings_file_path="embeddings/fictional_novel.csv",
+    text_embeddings_file_path="embeddings/fictional_novel.csv",
     question="主人公の性格は？"
 )
 print(f"Answer: {answer}")  # トシオは、ドジっぷりがありながらも、勇敢で、友情を大切にする性格である。
